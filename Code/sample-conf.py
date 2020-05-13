86733eaecd5d9aa8f854458503cfaf1d2f9fb6de
Soumik-Dhar <dev.soumik23@gmail.com>
2020-05-13 05:41:17 +0530

    update README and rename files

---
+++ Code/sample-conf.py
@@ -0,0 +1,9 @@
+# Telegram channel ID
+# CHAT_ID = @+channel permanent link name ( like - @testChannelLink1234 )
+CHAT_ID = "<insert your Telegram channel ID>"
+# Telegram bot API KEY
+# BOT_ID = bot+http api key (like - bot987637509:AAF5dQ8kbpVx4kKtd_Tn2cS1gFIxBrKYxF8)
+BOT_ID = "<insert your Telegram bot API KEY>"
+# Setting frame size and multiplication factor for Z-Score
+FRAME_SIZE = 10
+MUL_FACTOR = 3
