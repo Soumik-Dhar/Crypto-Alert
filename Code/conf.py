1fca769ee18226a0b7781b0a448352908ec99667
Soumik-Dhar <dev.soumik23@gmail.com>
2020-05-13 05:46:26 +0530

    hide conf.py

--- Code/conf.py
+++
@@ -1,7 +0,0 @@
-# Telegram channel ID
-CHAT_ID="@bitcoinalertnotif"
-# Telegram bot API KEY
-BOT_ID="bot1005785003:AAFU_LU5EJ_rwELCyFGlcLAyvccg4a1bWQI"
-# Setting frame size and multiplication factor for Z-Score
-FRAME_SIZE=10
-MUL_FACTOR=3
